2eb4d324fd4e225fef517888d50f32b8852b7219
Bahareh <mer_ir@yahoo.com>
2023-05-06 09:50:09 -0700

    add the route of create comment

--- controllers/api/comment-route.js
+++ controllers/api/comment-route.js
@@ -1,3 +1,22 @@
-// const router = require('express').Router();
-// const { Comment } = require('../../models');
-// module.exports = router;
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+const {formatCurrentDate} = require('../../utils/helpers');
+
+/*This route is responsible for creating a comment, it takes the comment as req.body and the id of the post as req.params
+and it adds user_id and current date to them and then saves the post in database*/
+router.post('/:id', withAuth, async (req,res) => {
+    try {
+        const commentData = await Comment.create({
+            comment: req.body.comment,
+            post_id: req.params.id,
+            date_created: formatCurrentDate(),
+            user_id: req.session.user_id
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-route.js');
 const postRoutes = require('./post-route.js');
-//const commentRoutes = require('./comment-route.js');
+const commentRoutes = require('./comment-route.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-//router.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
